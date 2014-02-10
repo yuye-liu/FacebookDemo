75365aa84a7f6d7553e2cfb410827bc2aaddee34
yuye-liu <yuye.liu@chukong-inc.com>
2014-02-10 16:08:09 +0800

    delete ignore folders

--- proj.android/gen/org/cocos2dx/FacebookDemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.cocos2dx.FacebookDemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /Users/liuyuye/Desktop/FacebookDemo/projects/FacebookDemo/proj.android/../../../cocos2dx/support/ccUtils.cpp \
- /Users/liuyuye/Desktop/FacebookDemo/projects/FacebookDemo/proj.android/../../../cocos2dx/support/ccUtils.h
-
-/Users/liuyuye/Desktop/FacebookDemo/projects/FacebookDemo/proj.android/../../../cocos2dx/support/ccUtils.h:
